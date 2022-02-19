119b16f80f556fd1d077c930986fc97fa3e2b749
surfer2517 <rybkin2517@mail.ru>
2022-02-19 16:57:13 +0300

    main

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Ivan Rybkin on 19.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello")
+    }
+
+}
